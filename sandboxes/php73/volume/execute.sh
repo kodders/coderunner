90bc440ff08626dd74a17c1dc896eea1bb0bd0c9
Taha Paksu <tpaksu@gmail.com>
2020-03-24 01:30:41 +0300

    Modify execute.sh to run tests

--- sandboxes/php73/volume/execute.sh
+++ sandboxes/php73/volume/execute.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cp php.ini /usr/local/etc/php
-php $1 > $2
+for i in `ls input*.txt | grep -oE '[0-9]+'`; do
+    php $1 < input_$i.txt > user_output_$i.txt
+    diff=`diff user_output_$i.txt output_$i.txt`
+    if [ ${#diff} -eq 0 ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+done
